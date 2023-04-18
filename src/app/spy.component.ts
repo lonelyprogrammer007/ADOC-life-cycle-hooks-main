644759d8601abe907e2aa18481bc85798707672a
lonelyprogrammer007 <108358225+lonelyprogrammer007@users.noreply.github.com>
2023-04-17 22:18:18 -0500

    1 file modified

--- src/app/spy.component.ts
+++ src/app/spy.component.ts
@@ -5,33 +5,28 @@ import { LoggerService } from './logger.service';
 @Component({
   selector: 'spy-parent',
   templateUrl: './spy.component.html',
-  providers:  [LoggerService]
+  providers: [LoggerService],
 })
 export class SpyParentComponent {
   newName = 'Herbie';
   heroes: string[] = ['Windstorm', 'Magneta'];
 
-  constructor(public logger: LoggerService) {
-  }
+  constructor(public logger: LoggerService) {}
 
   addHero() {
     if (this.newName.trim()) {
       this.heroes.push(this.newName.trim());
       this.newName = '';
-      this.logger.tick();
     }
   }
   removeHero(hero: string) {
     this.heroes.splice(this.heroes.indexOf(hero), 1);
-    this.logger.tick();
   }
   reset() {
     this.logger.log('reset');
     this.heroes = [];
-    this.logger.tick();
   }
 }
-
 
 /*
 Copyright Google LLC. All Rights Reserved.
